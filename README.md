817d2d03e061a8949c435c200546daebfd4f4c7f
ByronFanglei <984724098@qq.com>
2020-03-28 22:25:38 +0800

    首页导航

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# OnePlus
-一加官网
+# oneplus
+
+## 引入Bootstrapvue插件
+* npm引入
+```npm
+npm install vue bootstrap-vue bootstrap
+```
+* main.js需要引入
+```js
+//切记不能引入reset.css，否则字体会简称1rem，页面胖了好大一圈
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+```
---
+++ src/views/Home/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand href="#" class="iconfont" style="color:red">&#xe830;</b-navbar-brand>
+        <!-- <b-navbar-toggle target="nav-collapse"></b-navbar-toggle> -->
+        <b-collapse id="nav-collapse" is-nav>
+          <!-- <b-navbar-nav>
+            <b-nav-item href="#">Link</b-nav-item>
+            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          </b-navbar-nav> -->
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown right>
+              <template v-slot:button-content>
+                <em class="iconfont">&#xe602;</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+            <b-nav-item-dropdown right>
+              <template v-slot:button-content>
+                <em class="iconfont">&#xe65a;</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+
+</style>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
